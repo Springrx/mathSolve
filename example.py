b04e48ed4ec4600533cccc3722e95932cab98841
springrx <kangchunrx@163.com>
2024-07-05 18:21:42 +0800

    add: clear single choice data

---
+++ example.py
@@ -0,0 +1,15 @@
+{
+    "id": 0,
+    "question": "问题是: 函数$f\\left(x\\right)$是定义在R上的偶函数，且$f\\left(1+x\\right)=f\\left(1-x\\right)$，若$x\\in \\left[0,1\\right]$，$f\\left(x\\right)=2^{x}$，则$f\\left(2023\\right)=$（       ）\nA:4\nB:2\nC:1\nD:0",
+    "answer": {
+        "B": "2"
+    }
+},
+{
+        "question": "1. （5 分) 已知集合 $A=\\{x \\in R|| x \\mid \\leqslant 2\\}\\}, B=\\{x \\in Z \\mid \\sqrt{x} \\leqslant 4\\}$, 则 $A \\cap B=(\\quad$ ）\nA. $(0,2)$\nB. $[0,2]$\nC. $\\{0,2\\}$\nD. $\\{0,1,2\\}$\n",
+        "answer": [
+            "D"
+        ],
+        "analysis": "解: $A=\\{x \\in R|| x \\mid \\leqslant 2\\}=,\\{x \\in R \\mid-2 \\leqslant x \\leqslant 2\\}$,\n\n$B=\\{x \\in Z \\mid \\sqrt{x} \\leqslant 4\\}=\\{x \\in Z \\mid 0 \\leqslant x \\leqslant 16\\}$\n\n故 $A \\cap B=\\{0,1,2\\}$.\n\n应选 D.\n"
+    },
+
